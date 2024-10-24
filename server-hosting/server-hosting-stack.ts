58c13b2201a10c54f21e956bedc74c555ecae354
Arend Vosmaer <arendvosmaer@gmail.com>
2024-10-24 13:39:01 +0200

    Sorry for the mess

--- server-hosting/server-hosting-stack.ts
+++ server-hosting/server-hosting-stack.ts
@@ -1,14 +1,14 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { Config } from './config';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as s3_assets from 'aws-cdk-lib/aws-s3-assets';
-import * as lambda_nodejs from 'aws-cdk-lib/aws-lambda-nodejs';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Config } from "./config";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3_assets from "aws-cdk-lib/aws-s3-assets";
+import * as lambda_nodejs from "aws-cdk-lib/aws-lambda-nodejs";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
 
-export class ServerHostingStack extends Stack {
+export class SatisFactoryStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
@@ -23,74 +23,107 @@ export class ServerHostingStack extends Stack {
       // lookup vpc if given
       if (vpcId) {
         return ec2.Vpc.fromLookup(this, `${prefix}Vpc`, {
-          vpcId
-        })
+          vpcId,
+        });
 
         // use default vpc otherwise
       } else {
         return ec2.Vpc.fromLookup(this, `${prefix}Vpc`, {
-          isDefault: true
-        })
+          isDefault: true,
+        });
       }
-    }
+    };
 
-    let publicOrLookupSubnet = (subnetId: string, availabilityZone: string): ec2.SubnetSelection => {
+    let publicOrLookupSubnet = (
+      subnetId: string,
+      availabilityZone: string
+    ): ec2.SubnetSelection => {
       // if subnet id is given select it
       if (subnetId && availabilityZone) {
         return {
           subnets: [
-            ec2.Subnet.fromSubnetAttributes(this, `${Config.prefix}ServerSubnet`, {
-              availabilityZone,
-              subnetId
-            })
-          ]
+            ec2.Subnet.fromSubnetAttributes(
+              this,
+              `${Config.prefix}ServerSubnet`,
+              {
+                availabilityZone,
+                subnetId,
+              }
+            ),
+          ],
         };
 
         // else use any available public subnet
       } else {
         return { subnetType: ec2.SubnetType.PUBLIC };
       }
-    }
+    };
 
     const vpc = lookUpOrDefaultVpc(Config.vpcId);
-    const vpcSubnets = publicOrLookupSubnet(Config.subnetId, Config.availabilityZone);
+    const vpcSubnets = publicOrLookupSubnet(
+      Config.subnetId,
+      Config.availabilityZone
+    );
 
     // configure security group to allow ingress access to game ports
-    const securityGroup = new ec2.SecurityGroup(this, `${prefix}ServerSecurityGroup`, {
-      vpc,
-      description: "Allow Satisfactory client to connect to server",
-    })
+    const securityGroup = new ec2.SecurityGroup(
+      this,
+      `${prefix}ServerSecurityGroup`,
+      {
+        vpc,
+        description: "Allow Satisfactory client to connect to server",
+      }
+    );
 
-    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.udp(7777), "Game port UDP")
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.udp(7777),
+      "Game port UDP"
+    );
     // securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.udp(15000), "Beacon port UDP")
     // securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.udp(15777), "Query port UDP")
-    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(7777), "Game port TCP")
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(7777),
+      "Game port TCP"
+    );
     // securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(15000), "Beacon port TCP")
     // securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(15777), "Query port TCP")
+    securityGroup.addIngressRule(
+      ec2.Peer.ipv4("13.48.4.200/30"),
+      ec2.Port.tcp(22),
+      "SSH port"
+    );
 
     const server = new ec2.Instance(this, `${prefix}Server`, {
       // 2 vCPU, 8 GB RAM should be enough for most factories
       instanceType: new ec2.InstanceType("t3.xlarge"), //t3.xlarge (burstable) //c5a.2xlarge //c7i.2xlarge // previous: m7a.2xlarge ($$$)
-      creditSpecification:ec2.CpuCredits.STANDARD,
+      creditSpecification: ec2.CpuCredits.STANDARD,
       // get exact ami from parameter exported by canonical
       // https://discourse.ubuntu.com/t/finding-ubuntu-images-with-the-aws-ssm-parameter-store/15507
-      machineImage: ec2.MachineImage.fromSsmParameter("/aws/service/canonical/ubuntu/server/20.04/stable/current/amd64/hvm/ebs-gp2/ami-id"),
+      machineImage: ec2.MachineImage.fromSsmParameter(
+        "/aws/service/canonical/ubuntu/server/20.04/stable/current/amd64/hvm/ebs-gp2/ami-id"
+      ),
       // storage for steam, satisfactory and save files
       blockDevices: [
         {
           deviceName: "/dev/sda1",
-          volume: ec2.BlockDeviceVolume.ebs(30, {volumeType: ec2.EbsDeviceVolumeType.GP3}),
-        }
+          volume: ec2.BlockDeviceVolume.ebs(30, {
+            volumeType: ec2.EbsDeviceVolumeType.GP3,
+          }),
+        },
       ],
       // server needs a public ip to allow connections
       vpcSubnets,
       userDataCausesReplacement: true,
       vpc,
       securityGroup,
-    })
+    });
 
     // Add Base SSM Permissions, so we can use AWS Session Manager to connect to our server, rather than external SSH.
-    server.role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'));
+    server.role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")
+    );
 
     //////////////////////////////
     // Configure save bucket
@@ -99,13 +132,17 @@ export class ServerHostingStack extends Stack {
     let findOrCreateBucket = (bucketName: string): s3.IBucket => {
       // if bucket already exists lookup and use the bucket
       if (bucketName) {
-        return s3.Bucket.fromBucketName(this, `${prefix}SavesBucket`, bucketName);
+        return s3.Bucket.fromBucketName(
+          this,
+          `${prefix}SavesBucket`,
+          bucketName
+        );
         // if bucket does not exist create a new bucket
         // autogenerate name to reduce possibility of conflict
       } else {
         return new s3.Bucket(this, `${prefix}SavesBucket`);
       }
-    }
+    };
 
     // allow server to read and write save files to and from save bucket
     const savesBucket = findOrCreateBucket(Config.bucketName);
@@ -118,13 +155,19 @@ export class ServerHostingStack extends Stack {
     // add aws cli
     // needed to download install script asset and
     // perform backups to s3
-    server.userData.addCommands('sudo apt-get install unzip -y')
-    server.userData.addCommands('curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip && ./aws/install')
+    server.userData.addCommands("sudo apt-get install unzip -y");
+    server.userData.addCommands(
+      'curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip && ./aws/install'
+    );
 
     // package startup script and grant read access to server
-    const startupScript = new s3_assets.Asset(this, `${Config.prefix}InstallAsset`, {
-      path: './server-hosting/scripts/install.sh'
-    });
+    const startupScript = new s3_assets.Asset(
+      this,
+      `${Config.prefix}InstallAsset`,
+      {
+        path: "./server-hosting/scripts/install.sh",
+      }
+    );
     startupScript.grantRead(server.role);
 
     // download and execute startup script
@@ -135,7 +178,7 @@ export class ServerHostingStack extends Stack {
     });
     server.userData.addExecuteFileCommand({
       filePath: localPath,
-      arguments: `${savesBucket.bucketName} ${Config.useExperimentalBuild}`
+      arguments: `${savesBucket.bucketName} ${Config.useExperimentalBuild}`,
     });
 
     //////////////////////////////
@@ -143,28 +186,32 @@ export class ServerHostingStack extends Stack {
     //////////////////////////////
 
     if (Config.restartApi && Config.restartApi === true) {
-      const startServerLambda = new lambda_nodejs.NodejsFunction(this, `${Config.prefix}StartServerLambda`, {
-        entry: './server-hosting/lambda/index.ts',
-        description: "Restart game server",
-        timeout: Duration.seconds(10),
-        environment: {
-          INSTANCE_ID: server.instanceId
+      const startServerLambda = new lambda_nodejs.NodejsFunction(
+        this,
+        `${Config.prefix}StartServerLambda`,
+        {
+          entry: "./server-hosting/lambda/index.ts",
+          description: "Restart game server",
+          timeout: Duration.seconds(10),
+          environment: {
+            INSTANCE_ID: server.instanceId,
+          },
         }
-      })
+      );
 
-      startServerLambda.addToRolePolicy(new iam.PolicyStatement({
-        actions: [
-          'ec2:StartInstances',
-        ],
-        resources: [
-          `arn:aws:ec2:*:${Config.account}:instance/${server.instanceId}`,
-        ]
-      }))
+      startServerLambda.addToRolePolicy(
+        new iam.PolicyStatement({
+          actions: ["ec2:StartInstances"],
+          resources: [
+            `arn:aws:ec2:*:${Config.account}:instance/${server.instanceId}`,
+          ],
+        })
+      );
 
       new apigw.LambdaRestApi(this, `${Config.prefix}StartServerApi`, {
         handler: startServerLambda,
         description: "Trigger lambda function to start server",
-      })
+      });
     }
   }
 }
--- server-hosting/server-hosting.ts
+++ server-hosting/server-hosting.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { ServerHostingStack } from './server-hosting-stack';
-import { Config } from './config';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { SatisFactoryStack } from "./server-hosting-stack";
+import { Config } from "./config";
 
 const app = new cdk.App();
-new ServerHostingStack(app, 'ServerHostingStack', {
+new SatisFactoryStack(app, "SatisFactoryStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -16,7 +16,7 @@ new ServerHostingStack(app, 'ServerHostingStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: Config.account, region: Config.region},
+  env: { account: Config.account, region: Config.region },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
